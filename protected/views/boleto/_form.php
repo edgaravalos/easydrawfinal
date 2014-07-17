1c1469d7f7799df7c30d88d54bd2bacc5a2986b9
Claudiocfp <padron.claudio@gmail.com>
2014-07-17 10:48:04 -0500

    fecha actual en boleto

--- protected/views/boleto/_form.php
+++ protected/views/boleto/_form.php
@@ -59,7 +59,7 @@
 		<?php echo $form->labelEx($model,'fecha_venta'); ?>
             <?php date_default_timezone_set('UTC');       
                  $fecha= date("Y-m-d");
-	 echo $form->textField($model,'fecha_venta',array('readonly'=>'true',$model->fec_fac= $fecha)); ?>
+	 echo $form->textField($model,'fecha_venta',array('readonly'=>'true',$model->fecha_venta= $fecha)); ?>
             
 		<?php echo $form->error($model,'fecha_venta'); ?>
 	</div>
